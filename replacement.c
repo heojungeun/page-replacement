66b1528e686b8afe60a28e1005d33a693a05f2db
Julian Weisburd <jmweisburd@gmail.com>
2016-11-18 13:07:43 -0700

    Test print array

--- replacement.c
+++ replacement.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int[20] PAGES = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
-int PAGES_LENGTH = sizeof(PAGES)/sizeof(PAGES[0]);
 
 void main()
 {
+  length = sizeof(PAGES)/sizeof(PAGES[0]);
   int i;
-  for (i = 0; i < PAGES_LENGTH; ++i) {
+  for (i = 0; i < length; ++i) {
     printf("%i\n", PAGES[i]);
   }
 }
